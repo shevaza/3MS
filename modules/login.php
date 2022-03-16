2547cddea281ed6daa311bde77fbe93ca6c3d61e
shevaza <shafiq.zabet@gmail.com>
2022-03-16 23:42:29 +0200

    Update login.php

--- modules/login.php
+++ modules/login.php
@@ -12,40 +12,46 @@
 </head>
 
 <body class="bg-danger">
-        <nav class="navbar navbar-expand-sm navbar-light bg-light">
-            <div class="container-fluid">
-               <div class="row w-100 justify-content-center">
-                   <div class="col text-center">
-                       <img src="../res/img/logo.png" alt="" width="130">
-                   </div>
-               </div>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <div class="container-fluid">
+            <div class="row w-100 justify-content-center">
+                <div class="col text-center">
+                    <img src="../res/img/logo.png" alt="" width="130">
+                </div>
             </div>
-        </nav>
-        <div class="container mt-4">
-            <div class="row justify-content-center">
-                <div class="col-9">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="container">
+        </div>
+    </nav>
+    <div class="container mt-4">
+        <div class="row justify-content-center">
+            <div class="col col-lg-9 col-md-12 col-sm-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="container">
                             <div class="row">
-                                <div class="col">
-                                    <img src="" alt="" srcset="">
+                                <div class="col-12 col-lg-6 mb-3">
+                                    <img class="img-fluid" src="https://scontent.fbey7-1.fna.fbcdn.net/v/t31.18172-8/1291468_629393823781169_1550207425_o.jpg?_nc_cat=104&ccb=1-5&_nc_sid=19026a&_nc_ohc=UIf5CkCP-fcAX_wwSn6&_nc_ht=scontent.fbey7-1.fna&oh=00_AT8V3tyxT1eJP8wiHMWvISgOlX1bOzOQbqTOSb02QRAjjg&oe=6259A4E6" alt="">
                                 </div>
-                                <div class="col">
+                                <div class="col-12 col-lg-6">
                                     <h4 class="card-title text-center">Login</h4>
-                                        <div class="mb-3">
-                                          <label for="" class="form-label"></label>
-                                          <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
-                                          <small id="helpId" class="text-muted">Help text</small>
-                                        </div>
+                                    <div class="mb-1">
+                                        <label for="" class="form-label"></label>
+                                        <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+                                        <small id="helpId" class="text-muted">Username</small>
+                                    </div>
+                                    <div class="mb-1">
+                                        <label for="" class="form-label"></label>
+                                        <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+                                        <small id="helpId" class="text-muted">Password</small>
+                                    </div>
+                                        <button type="button" class="btn btn-primary">Login</button>
                                 </div>
-                            </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 
 </body>
 <?php
