de36234316fec8c47a22eb61cb04103409352c5a
shevaza <50750889+shevaza@users.noreply.github.com>
2022-03-16 16:32:45 +0200

    1

---
+++ css.php
@@ -0,0 +1,2 @@
+<link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+<link rel="stylesheet" href="../fontawesome/css/all.css">
---
+++ js.php
@@ -0,0 +1,4 @@
+<script src="../jquery/jquery.min.js"></script>
+<script src="../popper/popper.min.js"></script>
+<script src="../bootstrap/js/bootstrap.js"></script>
+<script src="../fontawesome/js/all.js"></script>
