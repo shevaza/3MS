a0b219e86b7fa048877e79024f6f52e21a9b47ee
shevaza <shafiq.zabet@gmail.com>
2022-10-19 16:08:03 +0300

    Update corrective.php

--- php_queries/corrective.php
+++ php_queries/corrective.php
@@ -27,25 +27,3 @@ if (isset($_POST['add'])) {
     header('Location: ../modules/maintenance/maintenance_corrective.php');
 
 }
-
-if (isset($_POST['edit'])) {
-    $check = mysqli_query($mysqli, "SELECT `id` FROM `machines` WHERE `number` LIKE '$_POST[machine_number]'");
-    if (mysqli_num_rows($check) > 0) {
-        header('Location: ../modules/setup/machines.php?err=Machine already exists.');
-    } else {
-        $insert = mysqli_query($mysqli, "INSERT INTO `machines` (`number`, `description`) 
-    VALUES ('$_POST[machine_number]', '$_POST[machine_description]')");
-        header('Location: ../modules/setup/machines.php');
-    }
-}
-
-if (isset($_POST['delete'])) {
-    $check = mysqli_query($mysqli, "SELECT `id` FROM `machines` WHERE `number` LIKE '$_POST[machine_number]'");
-    if (mysqli_num_rows($check) > 0) {
-        header('Location: ../modules/setup/machines.php?err=Machine already exists.');
-    } else {
-        $insert = mysqli_query($mysqli, "INSERT INTO `machines` (`number`, `description`) 
-    VALUES ('$_POST[machine_number]', '$_POST[machine_description]')");
-        header('Location: ../modules/setup/machines.php');
-    }
-}
